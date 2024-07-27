bfd8a91175c513bcd08beecb1b20615d7f9402ce
jonssonyan <yz808@outlook.com>
2024-07-27 16:28:07 +0800

    fix: cf proxy

--- docker_proxy.js
+++ docker_proxy.js
@@ -1,14 +1,21 @@
 export default {
-    async fetch(request, env, ctx) {
-        try {
-
-        } catch (error) {
-            console.log('Fetch error:', error);
-            return new Response('Internal Server Error', {status: 500});
-        }
+  async fetch(request, env, ctx) {
+    try {
+      const { TARGET_HOST = "docker" } = env;
+    } catch (error) {
+      console.log("Fetch error:", error);
+      return new Response("Internal Server Error", { status: 500 });
     }
-}
+  },
+};
 
 const routes = {
-
-}
+  docker: "https://registry-1.docker.io",
+  "k8s-gcr": "https://k8s.gcr.io",
+  k8s: "https://registry.k8s.io",
+  quay: "https://quay.io",
+  gcr: "https://gcr.io",
+  ghcr: "https://ghcr.io",
+  cloudsmith: "https://docker.cloudsmith.io",
+  ecr: "https://public.ecr.aws",
+};
--- http_proxy.js
+++ http_proxy.js
@@ -1,47 +1,77 @@
 export default {
-    async fetch(request, env, ctx) {
-        try {
-            const {TARGET_HOST, TARGET_PROTOCOL = "https", TARGET_PATHNAME_REGEX, UA_REGEX, URL302} = env;
-            const url = new URL(request.url);
-            const originUrlProtocol = url.protocol
-            const originUrlHostname = url.hostname
-            if (!TARGET_HOST || (TARGET_PATHNAME_REGEX && !new RegExp(TARGET_PATHNAME_REGEX).test(url.pathname)) || (UA_REGEX && !new RegExp(UA_REGEX).test(request.headers.get('user-agent').toLowerCase()))) {
-                console.log(`Invalid,clientIp:${request.headers.get('x-real-ip')},user-agent:${request.headers.get('user-agent')}`)
-                return URL302 ? Response.redirect(URL302, 302) : new Response('Invalid', {status: 400});
-            }
-            url.host = TARGET_HOST;
-            url.protocol = TARGET_PROTOCOL;
-            const newRequestHeaders = new Headers(request.headers);
-            newRequestHeaders.set('host', url.hostname);
-            newRequestHeaders.set('referer', request.url);
-            const originalResponse = await fetch(url.href, {
-                method: request.method,
-                headers: newRequestHeaders,
-                body: request.body || undefined
-            });
-            const newResponseHeaders = new Headers(originalResponse.headers);
-            newResponseHeaders.set('access-control-allow-origin', '*');
-            newResponseHeaders.set('access-control-allow-credentials', 'true');
-            newResponseHeaders.delete('content-security-policy');
-            newResponseHeaders.delete('content-security-policy-report-only');
-            newResponseHeaders.delete('clear-site-data');
-            const contentType = newResponseHeaders.get('content-type') || '';
-            let body;
-            if (contentType.includes('text/html')) {
-                body = await replaceResponseText(originalResponse, TARGET_HOST, TARGET_PATHNAME_REGEX, `${originUrlProtocol}//${originUrlHostname}`);
-            } else if (contentType.startsWith('image/') || contentType.startsWith('video/') || contentType.startsWith('audio/')) {
-                body = await originalResponse.arrayBuffer();
-            } else {
-                body = await originalResponse.blob();
-            }
-            return new Response(body, {
-                status: originalResponse.status, headers: newResponseHeaders
-            });
-        } catch (error) {
-            console.log('Fetch error:', error);
-            return new Response('Internal Server Error', {status: 500});
-        }
+  async fetch(request, env, ctx) {
+    try {
+      const {
+        TARGET_HOST,
+        TARGET_PROTOCOL = "https",
+        TARGET_PATHNAME_REGEX,
+        UA_REGEX,
+        URL302,
+      } = env;
+      const url = new URL(request.url);
+      const originUrlProtocol = url.protocol;
+      const originUrlHostname = url.hostname;
+      if (
+        !TARGET_HOST ||
+        (TARGET_PATHNAME_REGEX &&
+          !new RegExp(TARGET_PATHNAME_REGEX).test(url.pathname)) ||
+        (UA_REGEX &&
+          !new RegExp(UA_REGEX).test(
+            request.headers.get("user-agent").toLowerCase()
+          ))
+      ) {
+        console.log(
+          `Invalid,clientIp:${request.headers.get(
+            "x-real-ip"
+          )},user-agent:${request.headers.get("user-agent")}`
+        );
+        return URL302
+          ? Response.redirect(URL302, 302)
+          : new Response("Invalid", { status: 400 });
+      }
+      url.host = TARGET_HOST;
+      url.protocol = TARGET_PROTOCOL;
+      const newRequestHeaders = new Headers(request.headers);
+      newRequestHeaders.set("host", url.hostname);
+      newRequestHeaders.set("referer", request.url);
+      const originalResponse = await fetch(url.href, {
+        method: request.method,
+        headers: newRequestHeaders,
+        body: request.body || undefined,
+      });
+      const newResponseHeaders = new Headers(originalResponse.headers);
+      newResponseHeaders.set("access-control-allow-origin", "*");
+      newResponseHeaders.set("access-control-allow-credentials", "true");
+      newResponseHeaders.delete("content-security-policy");
+      newResponseHeaders.delete("content-security-policy-report-only");
+      newResponseHeaders.delete("clear-site-data");
+      const contentType = newResponseHeaders.get("content-type") || "";
+      let body;
+      if (contentType.includes("text/html")) {
+        body = await replaceResponseText(
+          originalResponse,
+          TARGET_HOST,
+          TARGET_PATHNAME_REGEX,
+          `${originUrlProtocol}//${originUrlHostname}`
+        );
+      } else if (
+        contentType.startsWith("image/") ||
+        contentType.startsWith("video/") ||
+        contentType.startsWith("audio/")
+      ) {
+        body = await originalResponse.arrayBuffer();
+      } else {
+        body = await originalResponse.blob();
+      }
+      return new Response(body, {
+        status: originalResponse.status,
+        headers: newResponseHeaders,
+      });
+    } catch (error) {
+      console.log("Fetch error:", error);
+      return new Response("Internal Server Error", { status: 500 });
     }
+  },
 };
 
 /**
@@ -52,11 +82,19 @@ export default {
  * @param str 替换的字符串
  * @returns {Promise<*>}
  */
-async function replaceResponseText(response, targetHost, targetPathnameRegex, str) {
-    let text = await response.text();
-    if (targetPathnameRegex) {
-        return text.replace(new RegExp(`^(https?://${targetHost})(${targetPathnameRegex})$`), `${str}$2`);
-    } else {
-        return text.replace(new RegExp(`^(https?://${targetHost})$`), str);
-    }
+async function replaceResponseText(
+  response,
+  targetHost,
+  targetPathnameRegex,
+  str
+) {
+  let text = await response.text();
+  if (targetPathnameRegex) {
+    return text.replace(
+      new RegExp(`^(https?://${targetHost})(${targetPathnameRegex})$`),
+      `${str}$2`
+    );
+  } else {
+    return text.replace(new RegExp(`^(https?://${targetHost})$`), str);
+  }
 }
