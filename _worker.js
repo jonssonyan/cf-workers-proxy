884c304efb3e8d00887d5c92fb67f7a07978af52
jonssonyan <yz808@outlook.com>
2024-07-28 22:15:11 +0800

    fix: nginx fake

--- _worker.js
+++ _worker.js
@@ -79,6 +79,32 @@ async function replaceResponseText(
   }
 }
 
+async function nginx() {
+  return `<!DOCTYPE html>
+<html>
+<head>
+<title>Welcome to nginx!</title>
+<style>
+html { color-scheme: light dark; }
+body { width: 35em; margin: 0 auto;
+font-family: Tahoma, Verdana, Arial, sans-serif; }
+</style>
+</head>
+<body>
+<h1>Welcome to nginx!</h1>
+<p>If you see this page, the nginx web server is successfully installed and
+working. Further configuration is required.</p>
+
+<p>For online documentation and support please refer to
+<a href="http://nginx.org/">nginx.org</a>.<br/>
+Commercial support is available at
+<a href="http://nginx.com/">nginx.com</a>.</p>
+
+<p><em>Thank you for using nginx.</em></p>
+</body>
+</html>`;
+}
+
 export default {
   async fetch(request, env, ctx) {
     try {
@@ -123,7 +149,11 @@ export default {
         logError(request, "Invalid");
         return URL302
           ? Response.redirect(URL302, 302)
-          : new Response("Invalid", { status: 400 });
+          : new Response(await nginx(), {
+              headers: {
+                "Content-Type": "text/html; charset=utf-8",
+              },
+            });
       }
       url.host = PROXY_HOSTNAME;
       url.protocol = PROXY_PROTOCOL;
